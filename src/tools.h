ff6075f413d4356161890b6036ffd6afb28e9d55
Ana Nora Evans <ananevans@gmail.com>
2019-03-12 01:55:37 -0400

    done

--- src/tools.h
+++ src/tools.h
@@ -5,27 +5,27 @@
 #include "Eigen/Dense"
 
 class Tools {
- public:
-  /**
-   * Constructor.
-   */
-  Tools();
+public:
+	/**
+	 * Constructor.
+	 */
+	Tools();
 
-  /**
-   * Destructor.
-   */
-  virtual ~Tools();
+	/**
+	 * Destructor.
+	 */
+	virtual ~Tools();
 
-  /**
-   * A helper method to calculate RMSE.
-   */
-  Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations, 
-                                const std::vector<Eigen::VectorXd> &ground_truth);
+	/**
+	 * A helper method to calculate RMSE.
+	 */
+	Eigen::VectorXd CalculateRMSE(const std::vector<Eigen::VectorXd> &estimations,
+			const std::vector<Eigen::VectorXd> &ground_truth);
 
-  /**
-   * A helper method to calculate Jacobians.
-   */
-  Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
+	/**
+	 * A helper method to calculate Jacobians.
+	 */
+	Eigen::MatrixXd CalculateJacobian(const Eigen::VectorXd& x_state);
 
 };
 
